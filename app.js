b17f61baeb25dd8eb8bc28a78bab0517765af5e5
Carlos Henry <carloshenry.wm@gmail.com>
2018-01-23 15:36:04 -0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRouter  = require('./user-routes')
+const app = express();
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+
+app.use((req, res, next) => {
+	console.log("Primeiro middleware");
+	next();
+});
+
+app.use(bodyParser.json());
+app.use('/user', userRouter);
+
+	/*, (req, res, next) => {
+	console.log("Segundo middleware");
+	next();
+});
+app.use((req, res, next) => {
+	console.log("Terceiro middleware");
+	next();
+});*/
+
+
+
+app.get("/", (req,res) => {
+	res.send("api running!");
+});
+app.get("/date", (req,res) =>{
+	var data = {
+		data : Date.now(),
+		resposta : "Success"
+	}
+	res.send(data);
+});
+app.get("/users/:user", (req,res) => {
+	var id = req.params.user;
+	var data = {
+		id : id
+	}
+	res.send(data);
+});
+
+app.post('/create', (req,res) => {
+	console.log(req.body.name);
+	var user = {
+		name : req.body.name,
+		pass : req.body.pass 
+	};
+	res.send(user);
+});
+
+var handleErros = (err,req,res, next) => { 
+	res.status(500).send("Algo está errado!");
+};
+app.use(handleErros);
+
+
+app.listen(PORT, () => {
+	console.log("Server running at http://%s:%s",HOST, PORT);
+});
---
+++ user-routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.all('*', (req, res, next) => {
+	console.log('User Routes');
+	next();
+});
+
+
+router.get('/', (req,res) => {
+	var data = {
+		resp : "user api!"
+	};
+	res.json(data);
+});
+
+router.post('/create', (req,res) => {
+	console.log(req.body.name+" saved!");
+	var user = {
+		name : req.body.name,
+		pass : req.body.pass 
+	};
+	res.send(user);
+});
+
+
+module.exports = router;
